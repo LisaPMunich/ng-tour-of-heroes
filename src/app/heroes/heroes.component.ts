cae6d4d41b651e224c1337ec6595e2a93fe780a4
LisaPape <lisa.pape@attempto.eu>
2022-12-15 21:12:29 +0100

    create a list of heroes and conditionally display details on click

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from '../hero';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -7,15 +8,14 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-  };
-
-  constructor() { }
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   ngOnInit(): void {
+  }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
